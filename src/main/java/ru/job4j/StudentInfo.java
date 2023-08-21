0bdaa1fb965d17b97145099320dc9256013b6f34
SapunovAU <squ1k@mail.ru>
2023-08-21 13:34:39 +0400

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,10 +2,10 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        int x = 7;
+        int x = 6;
         int y = 6;
         int rsl;
-        if (x > y) {
+        if (x == y) {
             rsl = x + y;
         } else {
             rsl = x * y;
