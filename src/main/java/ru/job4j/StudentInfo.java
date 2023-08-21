6ad2afcfe3ddab2d474748905c17d922b9a33cc0
SapunovAU <squ1k@mail.ru>
2023-08-21 10:38:26 +0400

    Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+            System.out.println("Sapunov Andrei");
+            System.out.println("18.09.1992");
+    }
+}
