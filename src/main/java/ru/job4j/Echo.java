c6038b40c94360f0eaa48fc554f93439374e284c
SapunovAU <squ1k@mail.ru>
2023-08-22 10:44:35 +0400

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,4 +1,14 @@
 package ru.job4j;
 
 public class Echo {
+    public static void main(String[] args) {
+        byte age = 18;
+        char gender = 'М';
+        float growth = 1.74f;
+        int qntBricks = 1257;
+        double qntBackterium = Math.pow(10, 7);
+        double distance = 156875;
+        boolean work = true;
+        long size = 927L;
+    }
 }
---
+++ src/main/java/ru/job4j/Tusk13.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+public class Tusk13 {
+        public static void main(String[] args) {
+double a = 4;
+double b = 4;
+double summa;
+double raznost;
+double umnozh;
+double del;
+summa = a + b;
+raznost = a - b;
+umnozh = a * b;
+del = a / b;
+System.out.println(summa);
+System.out.println(raznost);
+System.out.println(umnozh);
+System.out.print(del);
+        }
+    }
