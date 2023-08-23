9639ff0e525a6af1334d7a46df501a58cbe8f0ca
SapunovAU <squ1k@mail.ru>
2023-08-23 08:28:10 +0400

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -13,6 +13,10 @@ public class Point {
 
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
+        double result2 = Point.distance(6, 3, 1, 2);
+        double result3 = Point.distance(1, 8, 21, 6);
         System.out.println("result (0, 0) to (2, 0) " + result);
+        System.out.println("result (6, 3) to (1, 2) " + result2);
+        System.out.println("result (1, 8) to (21, 6) " + result3);
     }
 }
