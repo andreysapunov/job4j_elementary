324d9ac4d8e00ed9f8d9db8d1912331c5f0a1db6
SapunovAU <squ1k@mail.ru>
2023-08-23 13:57:22 +0400

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,20 +1,19 @@
 package ru.job4j.converter;
 
 public class Converter {
-
     public static float rubleToEuro(float value) {
-        float rsl = value * 70;
+        float rsl = value / 1;
         return rsl;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value * 60;
+        float rsl = value / 1;
         return rsl;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(1);
-        float dollar = Converter.rubleToDollar(1);
+        float euro = Converter.rubleToEuro(70);
+        float dollar = Converter.rubleToDollar(60);
         System.out.println("1 долар = " + dollar + " рублей.");
         System.out.println("1 евро = " + euro + " рублей.");
     }
