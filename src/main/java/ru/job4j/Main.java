e42781d3854e0eec2fc506800da0a4e5ccc48357
SapunovAU <squ1k@mail.ru>
2023-08-23 15:58:02 +0400

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,4 +1,11 @@
 package ru.job4j;
 
 public class Main {
+    public static void main(String[] args) {
+    int i = 100;
+    System.out.println("Use increment: " + i++);
+    System.out.println("After increment: " + i);
+    System.out.println("Use decrement: " + i--);
+    System.out.println("After decrement: " + i);
+    }
 }
--- src/main/java/ru/job4j/Tusk000.java
+++ src/main/java/ru/job4j/Tusk000.java
@@ -4,11 +4,13 @@ import java.io.Console;
 
 public class Tusk000 {
     public static void print(int x, int y) {
+        String peremen = x + " " + y;
+        System.out.println(peremen);
         System.out.println(x + y);
-        System.out.print(x * y);
+        System.out.println(x * y);
     }
 
     public static void main(String[] args) {
-    Tusk000.print(3, 5);
+    print(2, 53);
     }
 }
