d11a258ba53321c8744d243e41978b873da75def
SapunovAU <squ1k@mail.ru>
2023-08-24 15:07:05 +0400

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -8,9 +8,11 @@ public class Point {
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         double result2 = Point.distance(6, 3, 1, 2);
-        double result3 = Point.distance(1, 8, 21, 6);
+        double result3 = Point.distance(1, 8, 2, 6);
+        double result4 = Point.distance(2, 3, 7, 2);
         System.out.println("result (0, 0) to (2, 0) " + result);
         System.out.println("result (6, 3) to (1, 2) " + result2);
-        System.out.println("result (1, 8) to (21, 6) " + result3);
+        System.out.println("result (1, 8) to (2, 6) " + result3);
+        System.out.println("result (2, 3) to (7, 2) " + result4);
     }
 }
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void when00to20then2() {
+    double expected = 2;
+    int x1 = 0;
+    int y1 = 0;
+    int x2 = 2;
+    int y2 = 0;
+    double out = Point.distance(x1, y1, x2, y2);
+    assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+    @Test
+    void when63to12then509() {
+        double expected = 5.09;
+        int x1 = 6;
+        int y1 = 3;
+        int x2 = 1;
+        int y2 = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+    @Test
+    void when18to26then223() {
+        double expected = 2.23;
+        int x1 = 1;
+        int y1 = 8;
+        int x2 = 2;
+        int y2 = 6;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+    @Test
+    void when23to72then509() {
+        double expected = 5.09;
+        int x1 = 2;
+        int y1 = 3;
+        int x2 = 7;
+        int y2 = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
