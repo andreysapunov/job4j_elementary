f1428a1c7ad20cd4e9c9c0ec9592b9037cccc454
SapunovAU <squ1k@mail.ru>
2023-08-25 20:03:08 +0400

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,9 +2,9 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        int first = 6;
-        int second = 5;
-        boolean result = first != second;
-        System.out.println(result);
+        char a = 'A';
+        int b = 5;
+        int c = a + b;
+        System.out.println(c);
     }
 }
