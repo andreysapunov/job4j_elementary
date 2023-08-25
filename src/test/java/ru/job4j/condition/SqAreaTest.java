aa2441e3585442d69e4c14fc4235ecae3d5db163
SapunovAU <squ1k@mail.ru>
2023-08-25 13:52:00 +0400

    Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -6,11 +6,29 @@ import static org.assertj.core.api.Assertions.*;
 class SqAreaTest {
 
     @Test
-    void whenP6K2Square2() {
+    void whenP6K2Square2dot25() {
         double p = 6;
         double k = 1;
         double expected = 2.25;
         double out = SqArea.square(p, k);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
         }
+
+    @Test
+    void whenP9K6Square2dot47() {
+        double p = 9;
+        double k = 6;
+        double expected = 2.47;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        }
+
+    @Test
+    void whenP4K3Square0dot75() {
+        double p = 4;
+        double k = 3;
+        double expected = 0.75;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
     }
