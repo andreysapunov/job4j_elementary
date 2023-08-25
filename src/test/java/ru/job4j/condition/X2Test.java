28b2e4521529ef3c14a178ccc37baab09546da4c
SapunovAU <squ1k@mail.ru>
2023-08-25 13:26:47 +0400

    Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class X2Test {
+
     @Test
     void whenA10B0C0X2Then40() {
         int a = 10;
