5b004359b8ed6f09c99b59aa9d4928c2300fcd05
SapunovAU <squ1k@mail.ru>
2023-08-25 13:22:58 +0400

    Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -9,7 +9,7 @@ class SqAreaTest {
     void whenP6K2Square2() {
         double p = 6;
         double k = 1;
-        double expected= 2.25;
+        double expected = 2.25;
         double out = SqArea.square (p, k);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
         }
