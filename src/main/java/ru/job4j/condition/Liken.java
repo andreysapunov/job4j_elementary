83493530af843a23d6ba88094c6e997ef910981a
andreysapunov <squ1k@mail.ru>
2023-08-26 18:55:28 +0300

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -4,16 +4,15 @@ public class Liken {
     public static void main(String[] args) {
         int first = 10;
         int second = 9;
-
         boolean result = first > second;
         System.out.println(result);
         first = 7;
         second = 9;
-        boolean result2 = first < second;
-        System.out.println(result2);
+        result= first < second;
+        System.out.println(result);
         first = 3;
         second = 3;
-        boolean result3 = first == second;
-        System.out.println(result3);
+        result= first == second;
+        System.out.println(result);
     }
 }
