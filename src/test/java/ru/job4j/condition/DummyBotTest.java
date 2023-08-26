fdfc1116e27531908edb12852321112720dfade3
andreysapunov <squ1k@mail.ru>
2023-08-26 19:13:49 +0300

    Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/condition/DummyBotTest.java
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -3,6 +3,7 @@ package ru.job4j.condition;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 class DummyBotTest {
     @Test
     void whenGreetBot() {
