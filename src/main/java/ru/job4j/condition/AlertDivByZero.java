7e7d60d5d21ea23ae68b9338614ff436ff946f2f
andreysapunov <squ1k@mail.ru>
2023-08-26 19:02:22 +0300

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -4,10 +4,10 @@ public class AlertDivByZero {
 
     public static void possibleDiv(int number) {
         if (number == 0) {
-        System.out.println("Could not div by 0.");
+            System.out.println("Could not div by 0.");
         }
         if (number < 0) {
-        System.out.println("This is negative number");
+            System.out.println("This is negative number");
         }
     }
 
