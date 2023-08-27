1c38ea714df84c563d55f4ea0fd3a64147ffc573
andreysapunov <squ1k@mail.ru>
2023-08-27 13:51:00 +0300

    Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Max {
+
+    public static int max(int left, int right) {
+        int result = left >= right ? left : right;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int result1 = Max.max(20, 6);
+        int result2 = Max.max(5, 12);
+        int result3 = Max.max(6, 6);
+        System.out.println(result3);
+    }
+}
---
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MaxTest {
+
+    @Test
+    void whenMax1To2Then2() {
+        int left = 1;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    void whenMax15To19Then19() {
+        int left = 19;
+        int right = 15;
+        int result = Max.max(left, right);
+        int expected = 19;
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    void whenMax9To2Then9() {
+        int left = 9;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 9;
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    void whenMax6To6Then6() {
+        int left = 6;
+        int right = 6;
+        int result = Max.max(left, right);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+}
