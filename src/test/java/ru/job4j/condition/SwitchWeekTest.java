ebb55995aacf730fc51c86e0b45f3c7282fed398
andreysapunov <squ1k@mail.ru>
2023-08-28 14:49:24 +0300

    Фиксация изменений на github [#243564]

---
+++ src/test/java/ru/job4j/condition/SwitchWeekTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SwitchWeekTest {
+
+    @Test
+    void nameOfDay1itMonday() {
+    int day = 1;
+    String excepted = "Понедельник";
+    String out = SwitchWeek.nameOfDay(day);
+    assertThat(out).isEqualTo(excepted);
+    }
+
+    @Test
+    void nameOfDay6itSaturday() {
+        int day = 6;
+        String excepted = "Суббота";
+        String out = SwitchWeek.nameOfDay(day);
+        assertThat(out).isEqualTo(excepted);
+    }
+}
