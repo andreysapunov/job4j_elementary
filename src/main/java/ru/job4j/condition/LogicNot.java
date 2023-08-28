a8d00b94ced4b2b150006aff51e483c16637a6b9
andreysapunov <squ1k@mail.ru>
2023-08-28 12:56:56 +0300

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/LogicNot.java
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -4,6 +4,7 @@ public class LogicNot {
     public static boolean isEven(int num) {
         return num % 2 == 0;
     }
+
     public static boolean isPositive(int num) {
         return num > 0;
     }
