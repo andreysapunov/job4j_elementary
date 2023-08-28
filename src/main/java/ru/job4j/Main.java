9c32d3a3d3f52ca02b2707512e82b32b2f9bf194
andreysapunov <squ1k@mail.ru>
2023-08-28 14:50:23 +0300

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,10 +1,13 @@
 package ru.job4j;
 
 public class Main {
-    public static void main(String[] args) {
-        char a = 'A';
+    public static int main(String[] args) {
+        int a = 6;
         int b = 5;
-        int c = a + b;
-        System.out.println(c);
+        int c = 7;
+        if (a >= c) {
+            return a;
+        }
+        return b;
     }
 }
