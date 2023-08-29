c10d186d492e4a51285568716a1513cf9b2b6672
andreysapunov <squ1k@mail.ru>
2023-08-29 16:32:09 +0300

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -1,4 +1,5 @@
 package ru.job4j.loop;
+
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
@@ -8,11 +9,16 @@ public class Counter {
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
-        System.out.println(sum(6, 11));
-        System.out.println(sum(1, 4));
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                i = i;
+            } else {
+                i = i + 1;
+            }
+            sum = sum + i;
+        }
+        return sum;
     }
 }
---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CounterTest {
+
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromOneToSixThenTwelwe() {
+        int start = 1;
+        int finish = 6;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 12;
+        assertThat(result).isEqualTo(expected);
+    }
+}
