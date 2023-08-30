913f33afc8296a2f18d89c96b45ebd0acf95402a
andreysapunov <squ1k@mail.ru>
2023-08-30 12:23:42 +0300

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -3,11 +3,8 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calc(int n) {
         int fac = 1;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             fac = fac * i;
-            if (i == 0) {
-                fac = i;
-            }
         }
         return fac;
     }
