d84d946ee5c22d2545df02c3582d76c1ac52bf0d
andreysapunov <squ1k@mail.ru>
2023-08-31 18:11:44 +0300

    Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/loop/MortgageTest.java
+++ src/test/java/ru/job4j/loop/MortgageTest.java
@@ -2,7 +2,6 @@ package ru.job4j.loop;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-
 class MortgageTest {
     @Test
     void whenAmount1000Salary1200Percent1ThenYear1() {
