ea6bfb675f64543e1c12b9384d06ed72a3aa7ab2
SapunovAU <squ1k@mail.ru>
2023-09-01 08:08:19 +0400

    Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/loop/FitnessTest.java
+++ src/test/java/ru/job4j/loop/FitnessTest.java
@@ -2,7 +2,6 @@ package ru.job4j.loop;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-
 class FitnessTest {
     @Test
     void whenIvanGreatNik() {
