5d3aa92f9a1eaa94ccfd7147e076177e143c7881
andreysapunov <squ1k@mail.ru>
2023-09-02 12:52:11 +0300

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Tusk000.java
+++ src/main/java/ru/job4j/Tusk000.java
@@ -2,18 +2,14 @@ package ru.job4j;
 
 public class Tusk000 {
     public static String nameInsert(String name) {
-        return name;
+        String result = String.format("Привет, %s!", name);
+        System.out.println(result);
+        return result;
     }
 
     public static void main(String[] args) {
         String namePetr = Tusk000.nameInsert("Пётр");
         String nameIvan = Tusk000.nameInsert("Иван");
         String nameElena = Tusk000.nameInsert("Елена");
-        String result = String.format("Привет, %s!", namePetr);
-        String result2 = String.format("Привет, %s!", nameIvan);
-        String result3 = String.format("Привет, %s!", nameElena);
-        System.out.println(result);
-        System.out.println(result2);
-        System.out.println(result3);
     }
 }
