272e1317a50dd59f7d027c2d7af3d39db17a7974
SapunovAU <squ1k@mail.ru>
2023-09-06 07:51:14 +0400

    Фиксация изменений на github [#156317 #442662]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        for (int index = 0; index <  word.length; index++) {
-            if (word[word.length - 1] != post[post.length - 1]) {
+        for (int index = 0; index <  post.length; index++) {
+            if (word[word.length - index - 1] != post[post.length - index - 1]) {
                 result = false;
                 break;
             }
