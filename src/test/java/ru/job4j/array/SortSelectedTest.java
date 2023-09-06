5e2aa3058555eca3e250aee26dfd1a014d151206
SapunovAU <squ1k@mail.ru>
2023-09-06 15:09:24 +0400

    Фиксация изменений на github [#156320]

--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -2,6 +2,7 @@ package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+
 public class SortSelectedTest {
     @Test
     public void whenSortFive() {
