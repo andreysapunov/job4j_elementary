95cd134c28055f54aecdd1c1c108a085c8b86fca
SapunovAU <squ1k@mail.ru>
2023-09-07 11:40:30 +0400

    Фиксация изменений на github [#214126]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,7 +4,7 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int index = 0; index < board.length; index++) {
-                if (board[row][board[row].length - 1] == ' ') {
+               if (board[row][index] != 'X') {
                     result = false;
                     break;
             }
@@ -15,7 +15,7 @@ public class MatrixCheck {
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int index = 0; index < board.length; index++) {
-            if (board[column][column] == ' ') {
+            if (board[index][column] != 'X') {
                 result = false;
                 break;
             }
