20cabc0f9383a7dd6209d16616f776603875e471
SapunovAU <squ1k@mail.ru>
2023-09-12 19:57:52 +0400

    Фиксация изменений на github [#53859]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,10 +3,8 @@ package ru.job4j;
 public class Main {
 
     public static void main(String[] args) {
-        int income = 0;
-        while (income < 5) {
-            income++;
-            System.out.println(income);
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
         }
     }
 }
